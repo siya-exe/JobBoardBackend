0ddb5386829bb8ca404e34a2ad7f3d47cc3d04f0
siya <siyangwenya13@gmail.com>
2025-09-28 10:57:50 +0000

    Fix: Allowed host on setting.py

--- config/settings.py
+++ config/settings.py
@@ -171,3 +171,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import sys
+print("DEBUG ALLOWED_HOSTS =", ALLOWED_HOSTS, file=sys.stderr)
